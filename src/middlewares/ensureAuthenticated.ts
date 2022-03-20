d6656d51f7290c9858c5b4f0e42ce11134a22a75
Odilon Ferreira <odilon.aferreira@gmail.com>
2022-03-20 12:57:27 -0300

    creating a custom jwt payload

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+interface CustomJwtPayload {
+  // default value of the sub prop is a function, but in this application always will be a string (user id)
+  // take a look in AuthenticateUserService
+  sub: string;
+}
+
 export function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -16,7 +22,12 @@ export function ensureAuthenticated(
   const [, token] = authToken.split(" ");
 
   try {
-    const { sub } = verify(token, "ae365829bae78051232239c526e7687f");
+    const { sub } = verify(
+      token,
+      "ae365829bae78051232239c526e7687f"
+    ) as CustomJwtPayload;
+
+    request.user_id = sub;
 
     return next;
   } catch (error) {
